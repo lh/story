6fed56b9c0aacfd71f1d586c0ebdb7c4a45effa2
DomRowney <dominic.rowney@nhs.net>
2024-09-17 16:23:50 +0100

    In story created branch point for either left or right

--- Story_Time.py
+++ Story_Time.py
@@ -1,3 +1,10 @@
 print('Once upon a time there was a HSMA group called Zilog')
-choosen_fork = input('Choose a fork, left or right')
+print('They went down a load, and came across a fork')
+choosen_fork = input('Choose a fork in the road, left or right')
 
+if choosen_fork == 'left':
+    print(-f'you chose {chosen_fork}')
+    import story_fork_left
+if choosen_fork == 'right':
+    print(-f'you chose {chosen_fork}')
+    import story_fork_left
